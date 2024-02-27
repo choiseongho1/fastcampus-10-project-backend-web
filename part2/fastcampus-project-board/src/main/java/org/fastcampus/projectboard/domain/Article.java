d12eab5fc46356ce11a114dc22ed8a8aab3bef83
SeongHo <95_csh@naver.com>
2024-02-27 12:49:46 +0900

    Domain 클래스 설계 (게시글, 댓글)

---
+++ part2/fastcampus-project-board/src/main/java/org/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,17 @@
+package org.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title; // 제목
+    private String hashtag; // 해시태그
+    private String content; // 본문
+
+
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
