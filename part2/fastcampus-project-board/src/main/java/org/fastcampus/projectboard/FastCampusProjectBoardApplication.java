0d860073612ac9577a0a12674d4035a3d4e4467a
SeongHo <95_csh@naver.com>
2024-02-28 09:28:02 +0900

    @EnableJpaAuditing 어노테이션 추가

--- part2/fastcampus-project-board/src/main/java/org/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ part2/fastcampus-project-board/src/main/java/org/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -2,8 +2,12 @@ package org.fastcampus.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+
+//
+@EnableJpaAuditing
 public class FastCampusProjectBoardApplication {
 
 	public static void main(String[] args) {
