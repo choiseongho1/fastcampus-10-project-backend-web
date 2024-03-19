71424bb1dde23180066986102e3aee8e0698366d
SeongHo <95_csh@naver.com>
2024-03-19 15:37:08 +0900

    20240319
    multi-module 실습

---
+++ part6/multimodule/module-common/src/main/java/dev/be/modulecommon/services/CommonDemoService.java
@@ -0,0 +1,10 @@
+package dev.be.modulecommon.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonDemoService {
+    public String commonService(){
+        return "commonService";
+    }
+}
