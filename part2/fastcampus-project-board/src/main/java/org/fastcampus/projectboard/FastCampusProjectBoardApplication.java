2813d67b48b2d49bf9e17d5e6676459bf02eb898
SeongHo <95_csh@naver.com>
2024-03-25 08:21:50 +0900

    20240325

--- part2/fastcampus-project-board/src/main/java/org/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ part2/fastcampus-project-board/src/main/java/org/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -2,12 +2,11 @@ package org.fastcampus.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
-
-//
-@EnableJpaAuditing
 public class FastCampusProjectBoardApplication {
 
 	public static void main(String[] args) {
